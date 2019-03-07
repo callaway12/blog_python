a97763dd4649d86b3c2d41d69c4a7bf856be0449
callaway12 <taewankim0925@likelion.org>
2019-03-07 22:32:34 +0900

    no proc

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
-from .models import Blog
+import Blog
 # Create your views here.
 def home(request):
     blogs = Blog.objects
